a127ffbe359ff222267e28ad66951188b86c6104
alledelweiss <allena_salazar@dlsu.edu.ph>
2026-03-10 01:59:13 +0800

    hello world what is happening

---
+++ Frontends/Client-side/your-applications.php
@@ -0,0 +1,49 @@
+<?php 
+// CLIENT-SIDE
+// Displays the student's application form/s and its details
+// @alledelweiss
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Centralized Scholarship Program</title>
+        <link rel="stylesheet" href="your-applications.css">
+    </head>
+    <body>
+        <h3>[X] Centralized Scholarship Portal</h3>
+        <section>
+            <nav-1>
+                <!-- will edit once i know na 'yung file names mwa -->
+                <a href="#">Home</a> |
+                <a href="#">Scholarships</a> |
+                <a href="#">Your Applications</a> |
+                <a href="#">Profile</a>
+            </nav-1>
+            <h2>Your Applications</h2>
+            <b>Scholarship Name</b>
+            <p>Status: -----</p>
+            <p>Deadline: -----</p>
+            <p>Submission Date: -----</p>
+            <p>Scholarship ID: -----</p>
+            <a href="your-applications2.php">View Application</a>
+            <a href="application-form.php">Edit Application</a>
+        </section>
+
+        <br>
+            <section>
+                <b>Scholarship Name</b>
+                <p>Status: -----</p>
+                <p>Deadline: -----</p>
+                <p>Submission Date: -----</p>
+                <p>Scholarship ID: -----</p>
+                <a href="your-applications2.php">View Application</a>
+                <a href="application-form.php">Edit Application</a>
+            </section>
+        </br>
+
+        <footer>
+        <p>&copy; 2026 Centralized Scholarship Program</p>
+        </footer>
+    </body>
+</html>
---
+++ Frontends/Client-side/your-applications2.php
@@ -0,0 +1,58 @@
+<?php 
+// CLIENT-SIDE
+// Displays the student's application form/s and its details
+// @alledelweiss
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Centralized Scholarship Program</title>
+        <link rel="stylesheet" href="your-applications2.css">
+    </head>
+    <body>
+        <h3>[X] Centralized Scholarship Portal</h3>
+        <section>
+            <nav-1>
+                <!-- will edit once i know na 'yung file names mwa -->
+                <a href="#">Home</a> |
+                <a href="#">Scholarships</a> |
+                <a href="#">Your Applications</a> |
+                <a href="#">Profile</a>
+            </nav-1>
+            <b><h2>Scholarship Name</h2></b>
+            <p>Status: -----</p>
+            
+            <br>
+                <p>First Name: -----</p>
+                <p>Last Name: -----</p>
+                <p>Year Level: -----</p>
+                <p>Contact Number: -----</p>
+            </br>
+            
+            <br>
+                <p>Student ID: -----</p>
+                <p>Scholarship ID: -----</p>
+                <p>Application ID: -----</p>
+            </br>
+
+            <br>
+                <p>Document Type: -----</p>
+                <p>Document ID: -----</p>
+                <p>Submission Date: -----</p>
+            </br>
+
+            <br>
+                <p>Reviewed by: -----</p>
+                <p>Date reviewed: -----</p>
+            </br>
+
+            <a href="your-applications.php">Go Back</a>
+            <a href="application-form.php">Edit</a>
+        </section>
+
+        <footer>
+        <p>&copy; 2026 Centralized Scholarship Program</p>
+        </footer>
+    </body>
+</html>
