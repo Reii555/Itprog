592713ab1ddca7b1d27ba4d63d4ad5214334884b
Rei <SRLagoos@gmail.com>
2026-03-15 16:01:00 +0800

    hi

---
+++ Frontends/Server-side/AppMgmt.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <meta charset="UTF-8">
+    <title>Application Management</title>
+    <link rel="stylesheet" href="AppMgmt_Design.css">
+<body>
+    <div class="header">
+      <h3>Centralized Scholarship Portal / Admin Navigation / Application Management</h3>
+    </div>
+
+    <h1>Application Management</h1>
+
+    <div class="card-stats">
+      <div class="card">
+        <h2>Total Applications</h2>
+        <p>(insert value)</p>
+      </div>
+
+      <div class="card">
+        <h2>Pending Reviews</h2>
+        <p>(insert value)</p>
+      </div>
+
+      <div class="card">
+        <h2>Approved</h2>
+        <p>(insert value)</p>
+      </div>
+    </div>
+
+    <div class="filter-tab">
+        <select id="status" name="status">
+            <option value="all">All Statuses</option>
+            <option value="pending">Pending</option>
+            <option value="approved">Approved</option>
+            <option value="rejected">Rejected</option>
+        </select>
+    </div>
+    
+<!-- teka ayaw guman ng localhost/phpadmin kouh...-->
+
+</body>
+</html>
