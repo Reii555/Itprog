838e997d84eca273993bcc2e52027b958eb37f23
Rei <SRLagoos@gmail.com>
2026-03-18 22:19:30 +0800

    will finish the rest in the morning

--- Frontends/Server-side/Server_Dashboard.php
+++ Frontends/Server-side/Server_Dashboard.php
@@ -10,5 +10,37 @@
     </div>
 
     <h1>Dashboard</h1>
+
+    <section class="dashboard-stats-holder">
+        <section class="dashboard-stats">
+            <section class="card-total-scholarships">
+                <h4>Total Scholarships</h4>    
+                    <section class="stat-card">
+                        
+                    </section>
+            </section>
+
+            <section class="card-active">
+                <h4>Active</h4>    
+                    <section class="stat-card">
+                                    
+                     </section>
+            </section>
+
+            <section class="card-total-applicant">
+                <h4>Total Applicants</h4>
+                    <section class="stat-card">
+                        
+                    </section>
+            </section>
+
+            <section class="card-pending">
+                <h4>Pending</h4>
+                    <section class="stat-card">
+                        
+                    </section>
+            </section>
+        </section>
+    </section>
 </body>
     </html>
