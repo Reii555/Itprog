a7808e602a482ee8df8a6893bce7bb92086750a6
Rei <SRLagoos@gmail.com>
2026-04-02 17:26:18 +0800

    Gafgg

--- scholarship_db(POPULATED).sql
+++ scholarship_db(POPULATED).sql
@@ -20,10 +20,11 @@ SET time_zone = "+00:00";
 --
 -- Database: `scholarship_db`
 --
+  CREATE DATABASE IF NOT EXISTS `scholarship_db(POPULATED)` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+  USE `scholarship_db(POPULATED)`;
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `ACCOUNTS`
 --
 
